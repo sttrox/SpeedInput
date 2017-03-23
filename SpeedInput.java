8b8695f1a4c8bf7919c97b1307f13b5c22c107b9
sttrox <sttrox@mail.ru>
2017-03-23 15:16:51 +0300

    Close bug disappearances of zero

--- SpeedInput.java
+++ SpeedInput.java
@@ -6,10 +6,7 @@ import java.io.IOException;
 
 class SpeedInput
 {
-
-
     private static byte nul=0;
-
     private static byte setNulDec()
     {
         return SpeedInput.nul--;
@@ -20,20 +17,17 @@ class SpeedInput
         return ++SpeedInput.nul;
     }
 
-    static void getNul() //debug
-    {
-        System.out.println(nul);
-    }
+    private  static void setNul(byte t) { SpeedInput.nul=t; }
+
+    static boolean nullStart = true;
 
     private static int readIntNext(int num, int decimal) throws IOException {
         int temp = System.in.read();
-
         num*=decimal;
-        if (num==0)
-            setNul();
-
+        if (num > 0 ) nullStart = false;
+        if (num==0 && nullStart == false) setNul();
         if (temp >= 48 && temp <= 58){
-
+            setNul((byte) 0b0);
             num = num+readIntNext(temp^0b110000, decimal*10);
         }
         else
@@ -41,54 +35,43 @@ class SpeedInput
         return  num;
     }
 
-
     protected static int readInt() throws IOException {
-
-
+        nullStart = true;
         int num = System.in.read();
-
-        while (num <= 47 && num <= 58 )
+        while ((num <= 48 && num > 58))
         {
             num = System.in.read();
         }
-
         num^= 0b110000;
-
-        int temp = System.in.read();
-        if (num == 0 && temp == 0) return 0;
-
+        int temp = System.in.read(); //48
+        if (num>0) nullStart = false;
         final int decimal = 10  ;
         if (temp >= 48 && temp <= 58){
             num+= readIntNext(temp^0b110000, decimal );
             return revers(num);
         }
         else return  revers(num);
-
-
     }
 
     private static  int revers (int n)
     {
-
         if (n == 0) {
             return 0;
         }
         if  (SpeedInput.nul != 0 )
         {
             int temp = 1;
-            while (SpeedInput.nul !=0){
-                temp*=10;
+            while (SpeedInput.nul !=0) {
+                temp *= 10;
                 setNulDec();
             }
             return recursionNext(n, 0)*temp;
         }
-        else return recursionNext(n,0);
-
+        else
+            return recursionNext(n, 0);
     }
 
     private static int recursionNext(int n, int i) {
         return (n==0) ?  i : recursionNext( n/10, i*10 + n%10 );
     }
 }
-
-
